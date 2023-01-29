6e63c0b691f1eb2345136d67a6e117f127915f8f
angela.brunst <angela.hairstylist@gmail.com>
2023-01-28 17:24:29 -0700

    Trying to fix

--- api/app.js
+++ api/app.js
@@ -1,69 +1,9 @@
-// var createError = require('http-errors');
-const express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose = require('mongoose');
-
-mongoose.set('strictQuery', false);
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const api = require('./routes/api/index');
-
+var express = require('express');
 var app = express();
-
-mongoose.Promise = global.Promise;
-
-//Adds connection to database using mongoose
-mongoose.connect('mongodb+srv://abrunst:Jell.o2022@cluster0.qdazkmh.mongodb.net/appointments', {
-    useNewUrlParser: true
+var port = process.env.PORT || 3000;
+app.get('/', function(req, res) {
+    res.send('Welcome to my API!');
 });
-//This enabled CORS, Cross-origin resource sharing (CORS) is a mechanism that allows restricted resources (e.g. fonts) 
-//on a web page to be requested from another domain outside the domain from which the first resource was served
-app.all('/*', function(req, res, next) {
-    // CORS headers
-    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    // Set custom headers for CORS
-    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-    if (req.method == 'OPTIONS') {
-        res.status(200).end();
-    } else {
-        next();
-    }
+app.listen(port, function() {
+    console.log('Running on https://localhost/' + port);
 });
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/api', api);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-module.exports = app;
